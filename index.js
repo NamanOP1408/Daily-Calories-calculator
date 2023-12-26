1073b7b435cc78747905ce38e274e5ce521204e8
Naman <chauhannaman242@gmail.com>
2023-12-26 19:43:19 +0530

    Work in progress!

---
+++ index.js
@@ -0,0 +1,8 @@
+const caloriesCounter = getDocmentById('calories-counter');
+const budget = getDocmentById('budget');
+const newEntry = getDocmentById('new-entry');
+let isError = false;
+const addEntry = getDocmentById('add-entry');
+const calculate = getDocmentById('calculate');
+const reset = getDocmentById('reset');
+
