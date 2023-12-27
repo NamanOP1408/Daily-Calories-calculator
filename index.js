58897307c3b360d35b48f6ad9ba3f3a4deb80bfc
Naman <chauhannaman242@gmail.com>
2023-12-27 19:34:24 +0530

    Invalid input

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const addEntry = getDocmentById('add-entry');
 const calculate = getDocmentById('calculate');
 const reset = getDocmentById('reset');
 
-function isInvalidInput(str) {
+function clearInputString(str) {
     const regex = /[+-\s]/g;
     return regex.replace(str, '');
+}
+
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return regex.match(str);
+}
+
+function addEntry(){
+    
 }
